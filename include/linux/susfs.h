046c188b531ca86f782cb08e8186f39a8b281fa8
ppanzenboeck <panz.development@gmail.com>
2026-06-06 11:05:41 +0200

    Revert "fs: susfs: Better logging for SUS_PATH; Update ksu_susfs"
    This reverts commit f5f59b483eae61efecd745930f06509e7fd4f354.

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -27,7 +27,9 @@
 /* sus_path */
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 struct st_susfs_sus_path {
+	unsigned long                           target_ino;
 	char                                    target_pathname[SUSFS_MAX_LEN_PATHNAME];
+	unsigned int                            i_uid;
 	int                                     err;
 };
 
@@ -35,6 +37,7 @@ struct st_susfs_sus_path_list {
 	struct list_head                        list;
 	struct st_susfs_sus_path                info;
 	char                                    target_pathname[SUSFS_MAX_LEN_PATHNAME];
+	size_t                                  path_len;
 };
 #endif
 
